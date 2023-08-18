4af469367fc3253c4cac1f824629bf21fe770d9e
agvan <agvandyke25@gmail.com>
2023-08-18 14:08:56 -0500

    creating the Home.js and updated App.js and started building the Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,48 @@
-import logo from './/images/logo.png';
-import './App.css';
+import React, {useState} from "react";
+
+import Header from "./components/Header";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
+  const [options] = useState([{name: "Travel"}]);
+  const [currentOption, setOption] = useState(options[0]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Header
+      options={options}
+      setOption={setOption}
+      currentOption={currentOption}
+      />
+      <div>
+        <Routes>
+        <Route
+            path="/"
+            element={<Home
+            options={options}
+            setOption={setOption}
+            currentOption={currentOption}
+               />}
+          />
+          <Route path="/travel" element={<Travel/>} />
+          <Route path="/AllTravel" element={<AllTravel/>}/>
+          <Route path="/Boarding" element={<Boarding/>}/>
+          <Route
+          path="*"
+          element={<NoMatch/>}
+          />
+        </Routes>
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
+    </Router>
   );
 }
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,2 +1,49 @@
+// imports packages for react application
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+function Header() {
+  return (
+    <header className="col-12 d-inline-flex pb-3">
+      <div>
+        <Link to="/">
+          <h1
+            id="navheader"
+            className="d-inline-flex w-100 justify-content-center ms-4"
+          >
+            Woof's Up!
+          </h1>
+        </Link>
+
+        <div id="nav" className="mt-3">
+          <nav className="justify-content-center d-inline-flex flex-row">
+            
+            
+            <>
+              <Link
+                className="navlinks me-3 ms-4 mb-2"
+                to="/"
+                style={{ width: "5%", height: "15%" }}
+              >
+                Page
+              </Link>
+              
+              <Link
+                className="navlinks me-3 ms-4 mb-2"
+                to=""
+                style={{ width: "5%", height: "15%" }}
+              >
+                Page
+              </Link>
+              
+            </>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+// exports the Header component so that it can be used by the app
+export default Header;

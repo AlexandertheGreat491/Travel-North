a8989f64f35088b98bbee28dca069eb3664c142d
agvan <agvandyke25@gmail.com>
2023-08-19 11:58:06 -0500

    updated p element

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="d-inline-flex flex-wrap mt-2">
       <p className="m-2">Thanks for visiting!</p>
-      <p className="m-2">Check out my Github profiles:</p>
+      <p className="m-2">Check out my Github profile:</p>
       <div className="d-inline-flex">
         <a className="m-2" href="https://github.com/AlexandertheGreat491">
           <FaGithub /> Alexander Van Dyke

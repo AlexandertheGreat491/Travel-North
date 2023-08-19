d16c4d899d0bb4b6555ec7a5c2fd90a45e256ec5
agvan <agvandyke25@gmail.com>
2023-08-19 11:22:41 -0500

    removed unneeded code from footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,18 +10,8 @@ const Footer = () => {
         <a className="m-2" href="https://github.com/AlexandertheGreat491">
           <FaGithub /> Alexander Van Dyke
         </a>{" "}
-        <br></br>
-        <a className="m-2" href="https://github.com/NickSchenck">
-          <FaGithub /> Nick Schenck
-        </a>{" "}
-        <br></br>
-        <a className="m-2" href="https://github.com/willig0203">
-          <FaGithub /> Gary Williams
-        </a>{" "}
-        <br></br>
-        <a className="m-2" href="https://github.com/oliviamckee">
-          <FaGithub /> Olivia McKee
-        </a>
+        
+        
       </div>
     </footer>
   );

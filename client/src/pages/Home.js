9ffb013d0ef6e1ee62e7cd58f15f0ce58cd90fd7
agvan <agvandyke25@gmail.com>
2023-08-22 13:10:34 -0500

    changed margin around header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h2 className="homeheader">About Travel North</h2>
+            <h2 className="homeheader m-4">About Travel North</h2>
         </div>
     );
 };

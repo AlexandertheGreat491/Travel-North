b398d8a6936a1e13f59f097bf981b16178a903c2
agvan <agvandyke25@gmail.com>
2023-08-22 14:17:13 -0500

    fixed bootstrap & dependencies issues and added container class to p element & changed styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div className="about">
-        <div>
-            <h2 className="homeheader mb-3 ms-2 me-2">About Travel North</h2>
-        </div>
-        <div className="about">
-<p className="m-5">Hello</p>
-        </div>
-        </div>
-    );
+  return (
+    <div className="about">
+      <div>
+        <h2 className="homeheader mb-3 ms-2 me-2">About Travel North</h2>
+      </div>
+      <div className="about">
+        <p className="m-3 info justify-content-center container">
+          This is your one-stop shop for finding all of your information on
+          traveling and exploring northern locations in Minnesota, instead of
+          having to use dozens of different websites.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

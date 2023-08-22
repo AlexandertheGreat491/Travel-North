7c02347d39c592382f2979e33145678a1fa5a470
agvan <agvandyke25@gmail.com>
2023-08-22 13:21:21 -0500

    changed background colors and margins

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="d-inline-flex flex-wrap mt-2">
+    <footer className="d-inline-flex flex-wrap mt-2 foot">
       <p className="m-2">Thanks for visiting!</p>
       <p className="m-2">Check out my Github profile:</p>
       <div className="d-inline-flex">
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Home = () => {
     return (
+        <div className="about">
         <div>
-            <h2 className="homeheader m-4">About Travel North</h2>
+            <h2 className="homeheader mb-3 ms-2 me-2">About Travel North</h2>
+        </div>
+        <div className="about">
+<p className="m-5">Hello</p>
+        </div>
         </div>
     );
 };

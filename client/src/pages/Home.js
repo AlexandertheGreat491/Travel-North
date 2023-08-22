dfd52328dbdd81e06cfbb241e30b667e4f9104c0
agvan <agvandyke25@gmail.com>
2023-08-22 14:51:17 -0500

    changed margins and header alignment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="about" style={{height: '100vh'}}>
+    <div className="about square " style={{ height: "100vh" }}>
       <div>
-        <h2 className="homeheader mb-3 ms-2 me-2">About Travel North</h2>
+        <h2 className="homeheader border border-secondary rounded text-center">About Travel North</h2>
       </div>
-      <div className="about" >
-        <p className="m-3 info justify-content-center container">
+      <div className="about">
+        <p className="m-3 info justify-content-center container border border-secondary rounded">
           This is your one-stop shop for finding all of your information on
           traveling and exploring northern locations in Minnesota, instead of
           having to use dozens of different websites.
